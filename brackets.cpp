8a501c2ee94823e047c092f3a4c203488aa59bd1
David McKee <david.wayne.mckee@gmail.com>
2024-04-20 11:22:55 -0600

    Passes spec 1
    Running with "./check_brackets ${SOURCE_DIR}/*"
    returns outputs "Error in ../brackets.cpp" presumably because of the
    tests cases.
    
    Take note: there is no comment or string parsing going on here, so
    correct source files can fail.
    
    Take another note: I have to presume the location of the failure in
    brackets.cpp right now, but the next spec will fix that.

--- brackets.cpp
+++ brackets.cpp
@@ -15,37 +15,37 @@ using namespace std::string_literals;
 // All characters to process are here. If the value character is non-null it
 // represents the one open the key (which is a close) can consume.
 static const std::map<char, char> matches_for = {
-  {'(', '\0'}, {'[', '\0'}, {'{', '\0'},
-  { ')', '(' }, { ']', '[' }, { '}', '{' }
+    { '(', '\0' }, { '[', '\0' }, { '{', '\0' },
+    { ')', '(' }, { ']', '[' }, { '}', '{' }
 };
 
 brackets::status brackets::check(std::istream& is)
 {
     if (!is)
-      return status::stream_error;
-  
-  std::stack<char, std::vector<char>> stack;
-  
-  std::istream::char_type c;
+        return status::stream_error;
+
+    std::stack<char, std::vector<char>> stack;
+
+    std::istream::char_type c;
     while (is.get(c))
-      {
-      const auto it = matches_for.find(c);
-      if (it != matches_for.end())
-      {
-	if (it->second == '\0')
-	  stack.push(c);
-	else if (stack.empty())
-	  return status::extra_close;
-	else if (stack.top() == it->second)
-	  stack.pop();
-	else
-	  return status::mismatch;
-      }
+    {
+        const auto it = matches_for.find(c);
+        if (it != matches_for.end())
+        {
+            if (it->second == '\0')
+                stack.push(c);
+            else if (stack.empty())
+                return status::extra_close;
+            else if (stack.top() == it->second)
+                stack.pop();
+            else
+                return status::mismatch;
+        }
     }
 
     if (!stack.empty())
         return status::left_open;
-    
+
     return status::ok;
 }
 
@@ -54,41 +54,42 @@ TEST_CASE("Brackets matching")
     SUBCASE("correct")
     {
         std::map<std::string, std::string> data = {
-	  {"Empty input", ""},
-	  {"Sequential pairs", "()[]{}"},
-	  {"Nested pairs", "([{{()}}])"},
-	  {"Really mixed case", "([]{(()[])})"},
-	};
+            { "Empty input", "" },
+            { "Sequential pairs", "()[]{}" },
+            { "Nested pairs", "([{{()}}])" },
+            { "Really mixed case", "([]{(()[])})" },
+        };
 
         for (const auto& [key, value] : data)
-	  {
-              CAPTURE(key);
-              CAPTURE(value);
-	      std::istringstream is(value);
-	      CHECK(brackets::check(is) == brackets::status::ok);
-	  }
+        {
+            CAPTURE(key);
+            CAPTURE(value);
+            std::istringstream is(value);
+            CHECK(brackets::check(is) == brackets::status::ok);
+        }
     }
-    
+
     SUBCASE("mistakes")
     {
-        const std::vector<std::tuple <std::string,
-				      std::string,
-				      brackets::status>> data = {
-	{ "Lone close"s, ")"s, brackets::status::extra_close},
-	{ "Lone open"s, "["s, brackets::status::left_open},
-	{ "Simple mismatch"s, "{]"s, brackets::status::mismatch},
-	{ "Mis-ordered pair"s, "}{"s, brackets::status::extra_close},
-	{ "Mis-ordered nesting"s, "({)}"s, brackets::status::mismatch},
-	};
+        const std::vector<std::tuple<std::string,
+            std::string,
+            brackets::status>>
+            data = {
+                { "Lone close"s, ")"s, brackets::status::extra_close },
+                { "Lone open"s, "["s, brackets::status::left_open },
+                { "Simple mismatch"s, "{]"s, brackets::status::mismatch },
+                { "Mis-ordered pair"s, "}{"s, brackets::status::extra_close },
+                { "Mis-ordered nesting"s, "({)}"s, brackets::status::mismatch },
+            };
 
-      for (const auto& [key,
-			value,
-			expected_result] : data)
-	  {
-              CAPTURE(key);
-              CAPTURE(value);
-	      std::istringstream is(value);
-	      CHECK(brackets::check(is) == expected_result);
-	  }
+        for (const auto& [key,
+                 value,
+                 expected_result] : data)
+        {
+            CAPTURE(key);
+            CAPTURE(value);
+            std::istringstream is(value);
+            CHECK(brackets::check(is) == expected_result);
+        }
     }
 }
--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@
 
 int main(int argc, char* argv[])
 {
-    int status = EXIT_SUCCESS;
+    int final_status = EXIT_SUCCESS;
 
-    for (int i = 0; i < argc; ++i)
+    for (int i = 1; i < argc; ++i)
     {
         std::ifstream f(argv[i]);
-        if (brackets::check(f) != brackets::status::ok)
+        auto status = brackets::check(f);
+        if (status != brackets::status::ok)
         {
-            status = EXIT_FAILURE << 2;
+            final_status = EXIT_FAILURE;
             std::cerr << "Error in " << argv[i] << std::endl;
-            break;
         }
     }
 
-    return status;
+    return final_status;
 }
