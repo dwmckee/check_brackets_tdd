1378064c38f8f5c70e0d379b925d62ade45320d4
David McKee <david.wayne.mckee@gmail.com>
2024-04-27 17:25:01 -0600

    Improve and test error message
    Hand the bulk of error message generation to functions in the brackets
    namespace with the added benefit of making that code testable. Implement
    those tests.
    
    Some of the messages are still a little rough around the edges and the
    unneeded punctuation at the end of the stream error and all's well
    messages is annoying, but the code seems to work.
    
    Meets spec 3.

--- brackets.cpp
+++ brackets.cpp
@@ -24,7 +24,7 @@ static const std::map<brackets::status, std::string> status_strings = {
     { brackets::status::ok, "OK"s },
     { brackets::status::stream_error, "Bad stream"s },
     { brackets::status::extra_close, "Close symbol without open"s },
-    { brackets::status::left_open, "Reached end with unclosed symbols"s },
+    { brackets::status::left_open, "Reached end with open pair(s)"s },
     { brackets::status::mismatch, "Mismatch"s },
 };
 
@@ -49,6 +49,76 @@ std::istream& operator>>(std::istream& is, brackets::code_reference& cr)
     }
 
     return is;
+}
+
+std::string reference_location(const brackets::code_reference& ref,
+    std::string_view streamID)
+{
+    std::ostringstream ss;
+    ss << streamID << ":" << ref.line << ":" << ref.column;
+    return ss.str();
+}
+
+std::string brackets::result_report(const result& res, std::string_view streamID)
+{
+    constexpr auto eStr("error: ");
+
+    std::ostringstream ss;
+    ss << reference_location(res.error_ref, streamID)
+       << ": "
+       << (res.code == status::ok ? "" : eStr)
+       << status_string(res.code) << ": ";
+    switch (res.code)
+    {
+    case status::ok:
+    case status::stream_error:
+        // No additional data needed.
+        break;
+    case status::extra_close:
+        ss << "Found '" << res.error_ref.c
+           << "' with no matching (or, indeed, un-matching) open symbol.";
+        break;
+    case status::left_open:
+        ss << "Found end-of-file with open symbol '"
+           << res.back_ref.c << "' at "
+           << reference_location(res.back_ref, streamID) << ".";
+        break;
+    case status::mismatch:
+        ss << "Found '" << res.error_ref.c
+           << "' as putative match for '" << res.back_ref.c << "' at "
+           << reference_location(res.back_ref, streamID) << ".";
+        break;
+    }
+
+    return ss.str();
+}
+
+TEST_CASE("result reporting")
+{
+    using namespace brackets;
+
+    constexpr code_reference ref1 { 2, 2, 'A' };
+    constexpr code_reference ref2 { 1, 1, 'B' };
+    constexpr auto id = "ID";
+
+    const std::vector<std::tuple<result, std::string>> data = {
+        { { status::ok, ref1, ref2 }, "ID:2:2: OK: " },
+        { { status::stream_error, ref1, ref2 },
+            "ID:2:2: error: Bad stream: " },
+        { { status::extra_close, ref1, ref2 },
+            "ID:2:2: error: Close symbol without open: Found 'A' with no matching (or, indeed, un-matching) open symbol." },
+        { { status::left_open, ref1, ref2 },
+            "ID:2:2: error: Reached end with open pair(s): Found end-of-file with open symbol 'B' at ID:1:1." },
+        { { status::mismatch, ref1, ref2 },
+            "ID:2:2: error: Mismatch: Found 'A' as putative match for 'B' at ID:1:1." }
+    };
+
+    for (const auto& [res, expected] : data)
+    {
+        CAPTURE(res.code);
+        const auto actual = result_report(res, id);
+        CHECK(actual == expected);
+    }
 }
 
 brackets::result brackets::check(std::istream& is)
@@ -87,9 +157,9 @@ TEST_CASE("Brackets matching")
 {
     SUBCASE("correct")
     {
-      using namespace brackets;
+        using namespace brackets;
 
-      const std::vector<std::tuple<std::string, std::string, code_reference>> data = {
+        const std::vector<std::tuple<std::string, std::string, code_reference>> data = {
             { "Empty input", "", { 0, 1 } },
             { "Sequential pairs", "()[]{}", { 6, 1 } },
             { "Nested pairs", "([{{()}}])", { 10, 1 } },
@@ -110,17 +180,17 @@ TEST_CASE("Brackets matching")
 
     SUBCASE("mistakes")
     {
-      using namespace brackets;
-      
-        const std::vector < std::tuple < std::string,
+        using namespace brackets;
+
+        const std::vector<std::tuple<std::string,
             std::string,
-	    result>>
+            result>>
             data = {
-	  { "Lone close"s, ")"s, {status::extra_close, { 1, 1, ')'}} },
-	  { "Lone open"s, "["s, {status::left_open, { 1, 1, '['}, {1, 1, '['}} },
-	  { "Simple mismatch"s, "{]"s, {status::mismatch, { 2, 1, ']'}, {1, 1, '{'}} },
-	  { "Mis-ordered pair"s, "}{"s, {status::extra_close, { 1, 1, '}'}} },
-	  { "Mis-ordered nesting"s, "({)}"s, {status::mismatch, { 3, 1, ')'}, {2, 1, '{'}} },
+                { "Lone close"s, ")"s, { status::extra_close, { 1, 1, ')' } } },
+                { "Lone open"s, "["s, { status::left_open, { 1, 1, '[' }, { 1, 1, '[' } } },
+                { "Simple mismatch"s, "{]"s, { status::mismatch, { 2, 1, ']' }, { 1, 1, '{' } } },
+                { "Mis-ordered pair"s, "}{"s, { status::extra_close, { 1, 1, '}' } } },
+                { "Mis-ordered nesting"s, "({)}"s, { status::mismatch, { 3, 1, ')' }, { 2, 1, '{' } } },
             };
 
         for (const auto& [key, value, expected] : data)
