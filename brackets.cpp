77d4529d975cbc37dcda48be5e4bc28d053cd103
David McKee <david.wayne.mckee@gmail.com>
2024-04-27 14:02:46 -0600

    Refactoring in brackets

--- brackets.cpp
+++ brackets.cpp
@@ -33,63 +33,68 @@ std::string_view brackets::status_string(brackets::status s)
     return status_strings.at(s);
 }
 
+std::istream& operator>>(std::istream& is, brackets::code_reference& cr)
+{
+    if (is.get(cr.c))
+    {
+        if (cr.c == '\n')
+        {
+            ++cr.line;
+            cr.column = 0;
+        }
+        else
+	  {
+              ++cr.column;
+	  }
+      }
+
+      return is;
+  }
+
 brackets::result brackets::check(std::istream& is)
 {
     code_reference currentRef;
-    
+
     if (!is)
     {
-        return {status::stream_error, currentRef, {}};
+        return { status::stream_error, currentRef, {} };
     }
 
     std::stack<code_reference, std::vector<code_reference>> stack;
 
-    while (is.get(currentRef.c))
+    while (is >> currentRef)
     {
-        if (currentRef.c == '\n')
-        {
-            ++currentRef.line;
-            currentRef.column = 0;
-        }
-        else
-            ++currentRef.column;
-
         const auto it = matches_for.find(currentRef.c);
-        if (it != matches_for.end())
-        {
-            if (it->second == '\0') // open symbols are easy
-                stack.push(currentRef);
-            else if (stack.empty()) // close symbol with an empty stack
-            {
-                return {status::extra_close, currentRef, {}};
-            }
-            else if (stack.top().c == it->second) // correct pending open
-                stack.pop();
-            else
-            {
-                return {status::mismatch, currentRef, stack.top()};
-            }
-        }
+        if (it == matches_for.end()) // Not part of a pair
+	    ; // no-op
+	else if (it->second == '\0') // open symbols are easy
+	    stack.push(currentRef);
+	else if (stack.empty()) // close symbol with an empty stack
+	    return { status::extra_close, currentRef, {} };
+	else if (stack.top().c != it->second) // wrong close symbol
+	    return { status::mismatch, currentRef, stack.top() };
+	else // Right close symbol
+	    stack.pop();
     }
 
     if (!stack.empty())
-      return {status::left_open, currentRef, stack.top()};
+        return { status::left_open, currentRef, stack.top() };
 
-    return {status::ok, currentRef, {}};
+    return { status::ok, currentRef, {} };
 }
 
 TEST_CASE("Brackets matching")
 {
     SUBCASE("correct")
     {
-      const std::vector<std::tuple<std::string, std::string, brackets::code_reference>> data = {
-	{ "Empty input", "", {0, 1} },
-	{ "Sequential pairs", "()[]{}", {6, 1} },
-	{ "Nested pairs", "([{{()}}])", {10, 1} },
-	{ "Really mixed case", "(\n[]  {((\n\n)[]\n)})", {3, 5} },
+        const std::vector<std::tuple<std::string, std::string, brackets::code_reference>> data = {
+            { "Empty input", "", { 0, 1 } },
+            { "Sequential pairs", "()[]{}", { 6, 1 } },
+            { "Nested pairs", "([{{()}}])", { 10, 1 } },
+            { "Really mixed case", "(\n[]  {((\n\n)[]\n)})", { 3, 5 } },
         };
 
-      for (const auto& [key, value, ref] : data)
+        for (const auto& [key, value, ref] : data)
         {
             CAPTURE(key);
             CAPTURE(value);
@@ -103,16 +108,16 @@ TEST_CASE("Brackets matching")
 
     SUBCASE("mistakes")
     {
-        const std::vector < std::tuple < std::string,
+        const std::vector<std::tuple<std::string,
             std::string,
-					 brackets::status,
-					 brackets::code_reference>>
+            brackets::status,
+            brackets::code_reference>>
             data = {
-                { "Lone close"s, ")"s, brackets::status::extra_close, {1, 1} },
-                { "Lone open"s, "["s, brackets::status::left_open, {1, 1} },
-                { "Simple mismatch"s, "{]"s, brackets::status::mismatch, {2, 1} },
-                { "Mis-ordered pair"s, "}{"s, brackets::status::extra_close, {1, 1} },
-                { "Mis-ordered nesting"s, "({)}"s, brackets::status::mismatch, {3, 1} },
+                { "Lone close"s, ")"s, brackets::status::extra_close, { 1, 1 } },
+                { "Lone open"s, "["s, brackets::status::left_open, { 1, 1 } },
+                { "Simple mismatch"s, "{]"s, brackets::status::mismatch, { 2, 1 } },
+                { "Mis-ordered pair"s, "}{"s, brackets::status::extra_close, { 1, 1 } },
+                { "Mis-ordered nesting"s, "({)}"s, brackets::status::mismatch, { 3, 1 } },
             };
 
         for (const auto& [key, value, code, ref] : data)
--- brackets.hpp
+++ brackets.hpp
@@ -20,16 +20,16 @@ std::string_view status_string(status s);
 
 struct code_reference
 {
-  size_t column = 0;
-  size_t line = 1;
-  char c = '\0';
+    size_t column = 0;
+    size_t line = 1;
+    char c = '\0';
 };
-  
+
 struct result
 {
-  status code = status::ok;
-  code_reference error_ref;
-  code_reference back_ref;
+    status code = status::ok;
+    code_reference error_ref;
+    code_reference back_ref;
 };
 
 result check(std::istream& is);
